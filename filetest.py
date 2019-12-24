e5a623c5bc6c5134403a3c6a7a1332be5de24ea4
Victor Wang <11711201@mail.sustech.edu.cn>
2019-12-24 21:18:28 +0800

    remove dup files

--- filetest.py
+++
@@ -1,4 +0,0 @@
-import filecmp
-with open('./RT/out.out','r'), open('./ProblemData/1/1.out', 'r') as f1,f2:
-    print(f1.read() == f2.read()) 
-print(filecmp.cmp('./RT/out.out','./ProblemData/1/1.out',False))
